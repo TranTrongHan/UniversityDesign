b7894ea167317d06751f2f7e7eaa46b077a81bca
TranTrongHan <hintran6@gmail.com>
2023-07-30 15:19:54 +0700

    30thang7

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* CAROUSEL*/
 const buttons=document.querySelectorAll("[data-carousel-button]")
 
 buttons.forEach(button =>{
@@ -16,3 +17,19 @@ buttons.forEach(button =>{
         delete activeSlide.dataset.active
     })
 } )
+
+/* ONSCROLL */
+window.onscroll=function(){
+    console.info(document.documentElement.scrollTop)
+    if(document.documentElement.scrollTop>50||document.body.scrollTop>50){
+        var header=document.getElementById("myHeader")
+        header.style.position="fixed"
+        header.style.left=0
+        header.style.right=0
+        header.style.backgroundColor="rgba(29,85,159,0.8)"
+        header.style.zIndex=9
+    } else {
+        header.style.position="relative";
+        header.style.backgroundColor="rgb(29,85,159)"
+    }
+}
